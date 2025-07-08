ff8e6525978f8c9a004b82195533e0a996ab13d2
Pavel <pavel.oliynyk@gmail.com>
2025-07-08 15:46:12 +0100

    feat: initial commit

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocql/gocql"
+	"log"
+)
+
+// Ref: https://docs.aws.amazon.com/keyspaces/latest/devguide/using_go_driver.html
+func main() {
+
+	// add the Amazon Keyspaces service endpoint
+	cluster := gocql.NewCluster("localhost")
+	cluster.Port = 9142
+	// add your service specific credentials
+	cluster.Authenticator = gocql.PasswordAuthenticator{
+		Username: "ServiceUserName",
+		Password: "ServicePassword"}
+	// provide the path to the sf-class2-root.crt
+	//cluster.SslOpts = &gocql.SslOptions{
+	//	CaPath:                 "path_to_file/sf-class2-root.crt",
+	//	EnableHostVerification: false,
+	}
+
+	// Override default Consistency to LocalQuorum
+	cluster.Consistency = gocql.LocalQuorum
+	cluster.DisableInitialHostLookup = false
+
+	session, err := cluster.CreateSession()
+	if err != nil {
+		fmt.Println("err>", err)
+	}
+	defer session.Close()
+
+	// run a sample query from the system keyspace
+	var text string
+	iter := session.Query("SELECT keyspace_name FROM system_schema.tables;").Iter()
+	for iter.Scan(&text) {
+		fmt.Println("keyspace_name:", text)
+	}
+	if err := iter.Close(); err != nil {
+		log.Fatal(err)
+	}
+	session.Close()
+}
